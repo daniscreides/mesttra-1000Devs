cd0db7a729d6aa79442e5e051afda3da369b37c9
Dani <103336212+daniscreides@users.noreply.github.com>
2025-09-09 22:18:01 -0300

    Update MetroQuadrado.java

--- MetroQuadrado.java
+++ MetroQuadrado.java
@@ -1,3 +1,5 @@
+//Exercício 01: Uma imobiliária vende apenas terrenos retangulares. Faça um algoritmo para imprimir a área do terreno e o valor de venda do mesmo. Para isto será necessário o usuário informar as dimensões em metros (frente e lateral) do terreno além do valor cobrado pelo metro quadrado.
+
 import java.util.Scanner;
 
 public class MetroQuadrado {
